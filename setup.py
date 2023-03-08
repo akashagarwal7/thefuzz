33bb48717ae44793918129fc45c785fec2264832
Akash Agarwal <virusofminds@gmail.com>
2023-03-08 14:34:14 +1100

    fix: potential fix for imports for using the package directly from github

--- setup.py
+++ setup.py
@@ -18,27 +18,30 @@ def open_file(fname):
 
 
 setup(
-    name='akashagarwal7_thefuzz',
+    name="akashagarwal7_thefuzz",
     version=__version__,
-    author='Akash Agarwal',
-    author_email='agarwal.akash333@gmail.com',
-    packages=['akashagarwal7_thefuzz'], # Symbolic link to thefuzz to distinguish from the original package.
-    extras_require={'speedup': ['python-levenshtein>=0.12']},
-    url='https://github.com/akashagarwal7/thefuzz',
+    author="Akash Agarwal",
+    author_email="agarwal.akash333@gmail.com",
+    packages=[
+        "thefuzz",               # We need to keep this so we don't have to change the package internal import statements.
+        "akashagarwal7_thefuzz", # Symbolic link to thefuzz to distinguish from the original package.
+    ],
+    extras_require={"speedup": ["python-levenshtein>=0.12"]},
+    url="https://github.com/akashagarwal7/thefuzz",
     license="GPLv2",
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3 :: Only',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3 :: Only",
     ],
-    description='Fuzzy string matching in python, forked from SeatGeek\'s thefuzz',
-    long_description=open_file('README.rst').read(),
+    description="Fuzzy string matching in python, forked from SeatGeek's thefuzz",
+    long_description=open_file("README.rst").read(),
     zip_safe=True,
 )
